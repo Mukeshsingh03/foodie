1aba68e5c8df901785676cf104bf35a72e2f8fe9
Mukesh Singh <147894863+Mukeshsingh03@users.noreply.github.com>
2024-02-12 19:08:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,86 @@
+
+//banner change image
+function changeimg() {
+    document.querySelector("#bnr").src = "images/stir-clr.png";
+}
+
+function backimg() {
+    document.querySelector("#bnr").src = "images/stir.png";
+}
+//banner close image
+
+//menu seafood change image
+function changesea() {
+    document.querySelector("#sea").src = "images/SEAFOOD.png"
+}
+function backsea() {
+    document.querySelector("#sea").src = "images/seafood-outline.png"
+}
+//menu seafood change image
+
+//menu wrap change image
+function cngwrap() {
+    document.querySelector("#wrap").src = "images/WRAP.png"
+}
+function backwrap() {
+    document.querySelector("#wrap").src = "images/wrap-outline.png"
+}
+//menu wrap change image
+
+//menu salad change image
+function cngsalad() {
+    document.querySelector("#salad").src = "images/SALAD.png"
+}
+function backsalad() {
+    document.querySelector("#salad").src = "images/salad-outline.png"
+}
+//menu salad change image
+
+//menu biryani change image
+function cngbiryani() {
+    document.querySelector("#biryani").src = "images/BIRYANI.png"
+}
+function backbiryani() {
+    document.querySelector("#biryani").src = "images/biryani-outline.png"
+}
+//menu biryani change image
+
+
+//menu soup change image
+function cngsoup() {
+    document.querySelector("#soup").src = "images/SOUP.png"
+}
+function backsoup() {
+    document.querySelector("#soup").src = "images/soup-outline.png"
+}
+//menu soup change image
+
+
+//menu soup change image
+function cngpasta() {
+    document.querySelector("#pasta").src = "images/PASTA.png"
+}
+function backpasta() {
+    document.querySelector("#pasta").src = "images/pasta-outline.png"
+}
+//menu soup change image
+
+
+//menu dessert change image
+function cngdessert() {
+    document.querySelector("#dessert").src = "images/DESSERT.png"
+}
+function backdessert() {
+    document.querySelector("#dessert").src = "images/dessert-outline.png"
+}
+//menu dessert image
+
+//menu thai change image
+function cngthai() {
+    document.querySelector("#thai").src = "images/THAI.png"
+}
+function backthai() {
+    document.querySelector("#thai").src = "images/thai-outline.png"
+}
+//menu thai image
+
